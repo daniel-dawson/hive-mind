d8010346968a7c3ec9b37076c5e2f5edfe042b18
Daniel Dawson <dannyldawson@gmail.com>
2019-04-08 14:45:06 -0700

    Fix Entry migration to reflect fix in association

--- db/migrate/20190407205203_create_entries.rb
+++ db/migrate/20190407205203_create_entries.rb
@@ -1,7 +1,7 @@
 class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
-      t.integer :beekeeper_id
+      t.integer :hive_id
       t.float :temperature
       t.string :conditions
       t.string :content
