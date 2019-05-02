f8dcd1f77ec356bba58cb4581621270458348191
Daniel Dawson <dannyldawson@gmail.com>
2019-05-02 12:28:40 -0700

    Add route, logic, and view for adding a hive

--- app/controllers/hives_controller.rb
+++ app/controllers/hives_controller.rb
@@ -4,9 +4,29 @@ class HivesController < ApplicationController
     haml :'hives/index'
   end
 
+  get '/beekeepers/:username/hives/new' do |username|
+    @beekeeper = get_beekeeper_with_username_or_redirect username
+    haml :'hives/new'
+  end
+
+  post '/beekeepers/:username/hives' do |username|
+    beekeeper = get_beekeeper_with_username_or_redirect username
+    hive = beekeeper.hives.build(params[:hive])
+    if hive.save
+      redirect "/beekeepers/#{username}/hives/#{hive.name}"
+    else
+      flash[:errors] = hive.errors
+      redirect "/beekeepers/#{username}/hives/new"
+    end
+  end
+
   get '/beekeepers/:username/hives/:name' do |username, hive_name|
     @beekeeper = get_beekeeper_with_username_or_redirect username
     @hive = Hive.where(beekeeper_id: @beekeeper.id, name: hive_name).first
     haml :'hives/show'
+  end
+
+  patch '/beekeepers/:username/hives/:name' do |username, hive_name|
+
   end
 end
