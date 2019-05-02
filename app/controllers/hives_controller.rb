f517cc6177a41db5717d5d84b36a130425effe40
Daniel Dawson <dannyldawson@gmail.com>
2019-05-01 20:37:30 -0700

    Add show hive action/route

--- app/controllers/hives_controller.rb
+++ app/controllers/hives_controller.rb
@@ -3,4 +3,10 @@ class HivesController < ApplicationController
     @hives = Hive.all
     haml :'hives/index'
   end
+
+  get '/beekeepers/:username/hives/:name' do |username, hive_name|
+    @beekeeper = get_beekeeper_with_username_or_redirect username
+    @hive = Hive.where(beekeeper_id: @beekeeper.id, name: hive_name).first
+    haml :'hives/show'
+  end
 end
