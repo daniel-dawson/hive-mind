990412298b710d28a432eaa37bce55e47ea09094
Daniel Dawson <dannyldawson@gmail.com>
2019-05-03 04:24:55 -0700

    Make private helper method, refactor hive routes Private helper helps find a hive based on the user and hive name

--- app/controllers/hives_controller.rb
+++ app/controllers/hives_controller.rb
@@ -6,7 +6,12 @@ class HivesController < ApplicationController
 
   get '/beekeepers/:username/hives/new' do |username|
     @beekeeper = get_beekeeper_with_username_or_redirect username
-    haml :'hives/new'
+    if authorized? @beekeeper
+      haml :'hives/new'
+    else
+      flash[:notice] = "You are not allowed to make hives for this user"
+      redirect '/'
+    end
   end
 
   post '/beekeepers/:username/hives' do |username|
@@ -22,11 +27,29 @@ class HivesController < ApplicationController
 
   get '/beekeepers/:username/hives/:name' do |username, hive_name|
     @beekeeper = get_beekeeper_with_username_or_redirect username
-    @hive = Hive.where(beekeeper_id: @beekeeper.id, name: hive_name).first
+    @hive = get_hive_by_user_and_name @beekeeper, hive_name
     haml :'hives/show'
   end
 
-  patch '/beekeepers/:username/hives/:name' do |username, hive_name|
+  get '/beekeepers/:username/hives/:name/edit' do |username, hive_name|
+    @beekeeper = get_beekeeper_with_username_or_redirect username
+    @hive = get_hive_by_user_and_name @beekeeper, hive_name
+  end
 
+  patch '/beekeepers/:username/hives/:name' do |username, hive_name|
+    @beekeeper = get_beekeeper_with_username_or_redirect username
+    @hive = get_hive_by_user_and_name @beekeeper, hive_name
+    if @hive.update(params[:hive])
+      redirect "/beekeepers/#{username}/hives/#{hive_name}"
+    else
+      flash[:errors] = hive.errors.messages
+      redirect "/beekeepers/#{username}/hives/#{hive_name}/edit"
+    end
+  end
+
+  private
+
+  def get_hive_by_user_and_name(beekeeper, hive_name)
+    Hive.where(beekeeper_id: beekeeper.id, name: hive_name).first
   end
 end
